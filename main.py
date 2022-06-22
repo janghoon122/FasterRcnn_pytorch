74e1a65bb42f2d1162b0ca6157fa957ffb450020
DESKTOP-IU0RMSC\Janghoon <rltjdguswn@gmail.com>
2022-06-22 18:42:18 +0900

    Object information -display bound bvox and labels

--- main.py
+++ main.py
@@ -8,9 +8,37 @@ import numpy as np
 import cv2
 from PIL import Image
 
+
+# gpu configuration
 if(torch.cuda.is_available()):
     device = torch.device("cuda")
     print(device, torch.cuda.get_device_name(0))
 else:
     device=torch.device("cpu")
     print(device)
+
+
+'''
+Read a batch of training images along with their bounding boxes and labels.
+(In this example, we use read only 1 images, i.e., batch_size=1)
+'''
+
+# input image could be of any size
+img0 = cv2.imread('./image.jpg')
+img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
+print(img0.shape)
+plt.imshow(img0)
+plt.show()
+
+#Object information: a set of bounding boxes [ymin, xmin, ymax, xamx] and their labels
+bbox0 = np.array([[152, 1340, 204, 1470], [188, 1584, 243, 1693]])
+labels = np.array([1, 1]) #: background, 1: helmet
+
+#display bound box and labels
+img0_clone = np.copy(img0)
+for i in range(len(bbox0)):
+    cv2.rectangle(img0_clone, (bbox0[i][1], bbox0[i][0]), (bbox0[i][3], bbox0[i][2]), color=(0, 255, 0), thickness=3)
+    cv2.putText(img0_clone, str(int(labels[i])), (bbox0[i][3], bbox0[i][2]), cv2.FONT_HERSHEY_SIMPLEX, 3, (0,0,255), thickness=3)
+plt.imshow(img0_clone)
+plt.show()
+
