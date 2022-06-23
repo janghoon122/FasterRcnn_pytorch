058afadb08b07665c551513e4dfc933a70367fa4
DESKTOP-IU0RMSC\Janghoon <rltjdguswn@gmail.com>
2022-06-23 18:25:27 +0900

    pretrained VGG16 configuration

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ img0 = cv2.imread('./image.jpg')
 img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
 print(img0.shape)
 plt.imshow(img0)
-plt.show()
+# plt.show()
 
 #Object information: a set of bounding boxes [ymin, xmin, ymax, xamx] and their labels
 bbox0 = np.array([[152, 1340, 204, 1470], [188, 1584, 243, 1693]])
 labels = np.array([1, 1]) #: background, 1: helmet
 
-#display bound box and labels
+#display bounding box and labels
 img0_clone = np.copy(img0)
 for i in range(len(bbox0)):
     cv2.rectangle(img0_clone, (bbox0[i][1], bbox0[i][0]), (bbox0[i][3], bbox0[i][2]), color=(0, 255, 0), thickness=3)
@@ -53,3 +53,29 @@ for box in bbox0:
 bbox = np.array(bbox)
 print(bbox)
 
+'''
+Use VGG16 to extract features from input images
+input images (batch_size, H=800, W=800, d=3), Features:(batch_size, H=50, W=50, d=512)
+'''
+
+# List all the layers of VGG16
+model = torchvision.models.vgg16(pretrained=True).to(device)
+fe = list(model.features)
+print(len(fe))
+
+# collect layers with output feature map size (W, H) < 50
+dummy_img = torch.zeros((1, 3, 800, 800)).float() # test image array [1, 3, 800, 800]
+print(dummy_img.shape)
+
+req_features = []
+k = dummy_img.clone().to(device)
+for i in fe:
+    k = i(k)
+    if k.size()[2] < 800//16:
+        break
+    req_features.append(i)
+    out_channels = k.size()[1]
+print(len(req_features)) # 30
+print(out_channels) # 512
+
+# Conver this list into a Sequential module
